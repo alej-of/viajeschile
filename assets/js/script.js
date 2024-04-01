dc7f3ab30700e3a04a9ef5f6cb663890f7197ac6
Alej.OF <alej.of.19@gmail.com>
2024-04-01 19:45:48 -0300

    Agregado mensaje de alerta al presionar el boton de Enviar en el formulario de contacto, via jQuery.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+    $("#enviarCorreo").on("click", function () {
+        alert("Funcion deshabilitada temporalmente. Disculpe las molestias.");
+    });
+
 });
